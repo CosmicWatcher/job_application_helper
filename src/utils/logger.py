dde5e395b786d9e00732a1a1f12a10e254fa4b90
CosmicWatcher <aryan.kh88@gmail.com>
2025-03-14 13:27:48 -0400

    write logs to file

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,4 +1,25 @@
 from datetime import datetime, timezone
+import os
+from typing import Literal
+
+from config import ROOT_PATH
+
+
+class ANSIColors:
+    RESET = "\033[0m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[33m"
+
+
+def _get_color_code(level: Literal["INFO", "WARN", "ERROR"]):
+    match level:
+        case "INFO":
+            return ANSIColors.GREEN
+        case "WARN":
+            return ANSIColors.YELLOW
+        case "ERROR":
+            return ANSIColors.RED
 
 
 def _get_current_time():
@@ -9,16 +30,40 @@ def _get_current_time():
     return timestamp_string
 
 
-def info(message):
+def _print_to_console(level: Literal["INFO", "WARN", "ERROR"], message: str):
     current_time = _get_current_time()
-    print(f"\033[92m[INFO]\t{current_time} - {message}\033[00m")
+    color_code = _get_color_code(level)
+
+    print(f"{color_code}[{level}]\t{current_time} - {message}{ANSIColors.RESET}")
 
 
-def warn(message):
+def _save_to_file(level: Literal["INFO", "WARN", "ERROR"], message: str):
     current_time = _get_current_time()
-    print(f"\033[93m[WARN]\t{current_time} - {message}\033[00m")
+    color_code = _get_color_code(level)
+
+    log_dir = os.path.join(
+        ROOT_PATH,
+        "logs",
+    )
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, f"{datetime.now().strftime('%Y-%m-%d')}.log")
+    log_entry = f"{color_code}[{level}]\t{current_time} - {message}{ANSIColors.RESET}\n"
+
+    with open(log_file, "a") as f:
+        f.write(log_entry)
 
 
-def error(message):
-    current_time = _get_current_time()
-    print(f"\033[91m[ERROR]\t{current_time} - {message}\033[00m")
+def info(message: str):
+    _print_to_console("INFO", message)
+    _save_to_file("INFO", message)
+
+
+def warn(message: str):
+    _print_to_console("WARN", message)
+    _save_to_file("WARN", message)
+
+
+def error(message: str):
+    _print_to_console("ERROR", message)
+    _save_to_file("ERROR", message)
